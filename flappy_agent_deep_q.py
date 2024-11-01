06f66039265e21f277a5cf11ded5c9b3520b02e5
alexandersvafdal <alexandersvarfdal@gmail.com>
2024-11-01 12:01:47 +0000

    deep q agent does not suck now :D

--- flappy_agent_deep_q.py
+++ flappy_agent_deep_q.py
@@ -50,7 +50,7 @@ def run_game(nb_episodes, agent):
 def train(nb_episodes, agent):
     reward_values = agent.reward_values()
     
-    env = PLE(FlappyBird(), fps=30, display_screen=True, force_fps=True, rng=None,
+    env = PLE(FlappyBird(), fps=30, display_screen=False, force_fps=True, rng=None,
             reward_values = reward_values)
     env.init()
 
@@ -89,20 +89,29 @@ class FlappyAgentV2(FlappyAgent):
         self.frame_counter = 0
         
         # Hyperparameters
-        self.DISCOUNT_RATE_GAMMA = 0.9
-        self.INIT_LEARNING_RATE_ALPHA = 0.1
-        self.E_GREEDY_EPSILON = 0.1
+        self.DISCOUNT_RATE_GAMMA = 0.99         # Keep gamma < 1, otherwise it will only favor immediate rewards
+        self.INIT_LEARNING_RATE_ALPHA = 0.001   # Lower learning rate to avoid jumping over global minima
+        self.E_GREEDY_EPSILON = 0.1             # Consider starting 0.9 and use epsilon decay that decays epsilon every n amount of frames
+        self.MIN_EPSILON = 0.01                 # Have a minimum epsilon
         self.REPLAY_BUFFER_SIZE = 1000
-        self.BATCH_SIZE = 100
+        self.BATCH_SIZE = 64                    # Use a number that is a power of 2 because the CPU handles binaries faster
         self.MODEL_UPDATE_STEP = 100
     
         # Neural Network stuff
-        self.model = MLPRegressor(hidden_layer_sizes=(100, 10), activation="logistic",\
-                                  learning_rate_init=self.INIT_LEARNING_RATE_ALPHA, warm_start=True,\
-                                  max_iter=1)
-        self.target_model = MLPRegressor(hidden_layer_sizes=(100, 10), activation="logistic",\
-                                         learning_rate_init=self.INIT_LEARNING_RATE_ALPHA, warm_start=True,\
-                                         max_iter=1)
+        # self.model = MLPRegressor(hidden_layer_sizes=(100, 10), activation="logistic", learning_rate_init=self.INIT_LEARNING_RATE_ALPHA, warm_start=True)
+        # self.target_model = MLPRegressor(hidden_layer_sizes=(100, 10), activation="logistic", learning_rate_init=self.INIT_LEARNING_RATE_ALPHA, warm_start=True)
+
+        self.model = MLPRegressor(
+            hidden_layer_sizes=(128, 128, 128),
+            activation='relu',
+            learning_rate_init=self.INIT_LEARNING_RATE_ALPHA,
+            max_iter=1,
+            warm_start=True,
+            solver='adam',
+            learning_rate='adaptive',
+        )
+
+        self.target_model = deepcopy(self.model)
 
         # Fit network to dummies so as to get desired number for in- and output neurons
         dummy_input = np.zeros((1, 8))  # number of state attributes
@@ -142,6 +151,7 @@ class FlappyAgentV2(FlappyAgent):
         # return GameState(player_vel=vel, player_y=player_y, next_pipe_dist_to_player=x_dist_next, next_pipe_top_y=y_top_next,\
         #                  next_pipe_bottom_y=y_bottom_next, next_next_pipe_dist_to_player=x_dist_next_next,\
         #                     next_next_pipe_top_y=y_top_next_next, next_next_pipe_bottom_y=y_bottom_next_next)
+
         return (vel, player_y, x_dist_next, y_top_next, y_bottom_next, x_dist_next_next, y_top_next_next, y_bottom_next_next)
 
     def observe(self, s1, a, r, s2, end):
@@ -156,10 +166,10 @@ class FlappyAgentV2(FlappyAgent):
         # Normalize state 1 and state 2
         s1_norm = self.get_normalised_state_values(s1)
         s2_norm = self.get_normalised_state_values(s2)
+
         # Append the state, action, reward, state 2 and end in bottom
         self.replay_buffer.append((s1_norm, a, r, s2_norm, end))
 
-        print("len replay_buffer: ", len(self.replay_buffer))
         if len(self.replay_buffer) >= self.BATCH_SIZE:
             # Draw random sample from the replay buffer
             batch = random.sample(self.replay_buffer, self.BATCH_SIZE)
@@ -169,33 +179,27 @@ class FlappyAgentV2(FlappyAgent):
             # Convert to Numpy for efficiency
             s1_batch = np.array(s1_batch)
             a_batch = np.array(a_batch)
-            r_batch = np.array(r_batch)#.reshape(-1, 1)  # gets an array of arrays, supposedly required for fitting function
+            r_batch = np.array(r_batch).reshape(-1, 1)  # gets an array of arrays, supposedly required for fitting function
             s2_batch = np.array(s2_batch)
-            end_batch = np.array(end_batch)#.reshape(-1, 1)  
+            end_batch = np.array(end_batch).reshape(-1, 1)  
             
             q_s1 = self.model.predict(s1_batch)
             q_s2 = self.target_model.predict(s2_batch)
-            # print("q_s1 (batch): ", q_s1)
 
 
-            # target_q_values = q_s1.copy()
+            target_q_values = q_s1.copy()
             # Taking the maximum expected benefit of future state (Q learning), also ensuring the right format for the for loop later
-            max_q_values_s2 = np.max(q_s2, axis=1)#.reshape(-1,1)
+            max_q_values_s2 = np.max(q_s2, axis=1).reshape(-1,1)
 
             for i in range(self.BATCH_SIZE):
                 if end_batch[i]:  # if state is terminal, use reward only
-                    q_s1[i, a_batch[i]] = r_batch[i]
+                    target_q_values[i, a_batch[i]] = r_batch[i]
                 else:
-                    q_s1[i, a_batch[i]] = r_batch[i] + self.DISCOUNT_RATE_GAMMA * max_q_values_s2[i]
-                    # print("UPDATE TARGET Q VALUES: ", target_q_values[i, a_batch[i]])
+                    target_q_values[i, a_batch[i]] = r_batch[i] + self.DISCOUNT_RATE_GAMMA * max_q_values_s2[i]
             # Partially fitting the model because its more memory efficient
-            self.model.partial_fit(s1_batch, q_s1)
-            print("JUST PARTIALLY FIT THE MODEL")
-            print("Min Q-value in q_s1:", np.min(q_s1), "Max Q-value in q_s1:", np.max(q_s1))
-            print("Min Q-value in q_s2:", np.min(q_s2), "Max Q-value in q_s2:", np.max(q_s2))
+            self.model.partial_fit(s1_batch, target_q_values)
+      
 
-       
-        print("self.frame_counter: ", self.frame_counter)
         # count the frames, so as to move the model into the target model after x steps
         if self.frame_counter >= self.MODEL_UPDATE_STEP:
             print(f"UPDATE TARGET MODEL, frame counter is {self.frame_counter}")
@@ -238,18 +242,13 @@ class FlappyAgentV2(FlappyAgent):
         return np.argmax(q_values)
 
     def update_target_model(self):
-        # self.target_model.coefs_ = deepcopy(self.model.coefs_)
-        # self.target_model.intercepts_ = deepcopy(self.model.intercepts_)
-        print("std. coeffs: ", self.model.coefs_[0][0][:5])
-        print("target coeffs: ", self.target_model.coefs_[0][0][:5])
-        self.target_model.coefs_ = [np.copy(coef) for coef in self.model.coefs_]
-        self.target_model.intercepts_ = [np.copy(intercept) for intercept in self.model.intercepts_]
-        print("std. coeffs UPDTATE: ", self.model.coefs_[0][0][:5])
-        print("target coeffs UPDATE: ", self.target_model.coefs_[0][0][:5])
+        self.target_model.coefs_ = deepcopy(self.model.coefs_)
+        self.target_model.intercepts_ = deepcopy(self.model.intercepts_)
 
-if __name__ == "__main__":
-    agent = FlappyAgentV2()
-    train(1000, agent)
-    input("Press any button to resume to playing (training mode off)")
-    run_game(10, agent)
+
+
+agent = FlappyAgentV2()
+train(2000, agent)
+input("Press any button to resume to playing (training mode off)")
+run_game(100, agent)
 
